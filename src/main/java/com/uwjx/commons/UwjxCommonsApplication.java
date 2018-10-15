724b7c9ffaa695ab4347ca0a8d33d8cfafda2af5
Hugh Wang <18501667737@163.com>
2018-10-15 14:58:20 +0800

    push on branch master

---
+++ src/main/java/com/uwjx/commons/UwjxCommonsApplication.java
@@ -0,0 +1,12 @@
+package com.uwjx.commons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UwjxCommonsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UwjxCommonsApplication.class, args);
+    }
+}
---
+++ src/main/java/com/uwjx/commons/utils/TextUtil.java
@@ -0,0 +1,4 @@
+package com.uwjx.commons.utils;
+
+public class TextUtil {
+}
